88ea045ebc344ef94875baa3d1af9d641a8fa6d2
Jinzhao Yu <kevinyu0904@126.com>
2023-02-06 22:15:16 -0500

    test_first

---
+++ CodingBook/2068.检查两个字符串是否几乎相等.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode.cn id=2068 lang=python
+#
+# [2068] 检查两个字符串是否几乎相等
+#
+
+# @lc code=start
+class Solution(object):
+    def checkAlmostEquivalent(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: bool
+        """
+# @lc code=end
+
