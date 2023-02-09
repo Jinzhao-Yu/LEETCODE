608a9690ecdf174fdb1269eba1bf4598797fcab1
Jinzhao Yu <kevinyu0904@126.com>
2023-02-09 01:17:23 -0500

    02/09/2023

---
+++ CodingBook/704.二分查找.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode.cn id=704 lang=python
+#
+# [704] 二分查找
+#
+
+# @lc code=start
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l,r = 0,len(nums)-1
+        while l <= r:
+            mid = l + (r-l)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid+1
+            else:
+                r = mid-1
+        return -1
+# @lc code=end
+
