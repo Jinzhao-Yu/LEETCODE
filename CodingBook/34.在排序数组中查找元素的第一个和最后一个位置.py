a6c3b8e0b78c24be1081ea0d6b8caca101113179
Jinzhao Yu <kevinyu0904@126.com>
2023-02-20 23:26:58 -0500

    Feb20, LC34

--- CodingBook/34.在排序数组中查找元素的第一个和最后一个位置.py
+++ CodingBook/34.在排序数组中查找元素的第一个和最后一个位置.py
@@ -12,8 +12,68 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        """
+        Method1: Binary Search find one target, and then slide window to find left and right
+        """
+        # def binarySearch(nums, target):
+        #     l,r = 0, len(nums)-1
+        #     while l <= r:
+        #         mid = l + (r-l)//2
+        #         if nums[mid] == target:
+        #             return mid
+        #         elif nums[mid] > target:
+        #             r = mid-1
+        #         else:
+        #             l = mid+1
+        #     return -1
+        
+        # idx = binarySearch(nums, target)
+        # if idx == -1:
+        #     return [-1,-1]
+        # left, right = idx, idx
+        # while left >= 0 and nums[left] == target:
+        #     left -= 1
+        # while right < len(nums) and nums[right] == target:
+        #     right += 1
+        # return [left+1, right-1]
+
+        """
+        Method2: Find Left border and Right border seperately
+        """
+        def findLeft(nums, target):
+            leftBorder = -2
+            l, r = 0, len(nums)-1
+            while l <= r:
+                mid = l + (r-l)//2
+                if nums[mid] < target:
+                    l = mid+1
+                else:
+                    r = mid-1
+                    leftBorder = r
+            return leftBorder
         def findRight(nums, target):
-            l,r = 
+            rightBorder = -2
+            l, r = 0, len(nums)-1
+            while l <= r:
+                mid = l + (r-l)//2
+                if nums[mid] > target:
+                    r = mid-1
+                else:
+                    l = mid+1
+                    rightBorder = l
+            return rightBorder
+        
+        left, right = findLeft(nums, target), findRight(nums, target)
+        if left == -2 or right == -2:
+            return [-1,-1]
+        elif right - left > 1: 
+            return [left+1, right-1]
+        else:
+            return [-1,-1]
+
+
+                
+
 
 # @lc code=end
 
