4d7b867f545a50112415f8875d497c037b1a9a0c
Jinzhao Yu <kevinyu0904@126.com>
2023-02-21 20:57:54 -0500

    Feb21 - LC27

---
+++ CodingBook/27.移除元素.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode.cn id=27 lang=python
+#
+# [27] 移除元素
+#
+
+# @lc code=start
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        fast, slow = 0, 0
+        while fast < len(nums):
+            if nums[fast] == val:
+                fast += 1
+            else:
+                nums[slow] = nums[fast]
+                slow += 1
+                fast += 1
+        return slow
+
+# @lc code=end
+
