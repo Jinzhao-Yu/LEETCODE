736f03ce0bcd47e1fda38d2f4d028dcccd74fb90
Jinzhao Yu <kevinyu0904@126.com>
2023-02-22 21:50:38 -0500

    Feb22 - LC283

---
+++ CodingBook/283.移动零.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode.cn id=283 lang=python
+#
+# [283] 移动零
+#
+
+# @lc code=start
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        slow, fast = 0, 0
+        while fast < len(nums):
+            if nums[fast] != 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+                slow += 1
+            fast += 1
+        
+        return nums
+# @lc code=end
+
